2524b0b15b9a3d2839986f4be22b49638a57219b
faniriantsoa <jrazafinoelison@ingenosya.mg>
2026-02-03 09:47:16 +0100

    integration portfolio faniriantsoa

---
+++ js/global.js
@@ -0,0 +1,38 @@
+// $(document).ready(function () {
+
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('#v-pills-tab .nav-link');
+    let index = 0;
+
+    setInterval(() => {
+        index = (index + 1) % tabs.length;
+
+        const tab = new bootstrap.Tab(tabs[index]);
+        tab.show();
+    }, 5000); // 5 secondes
+});
+
+$(document).ready(function () {
+    $('.scroll-link').on('click', function (e) {
+
+        if (window.location.hash === target) {
+            // Ne pas empêcher le comportement
+            return;
+        }
+
+        e.preventDefault();
+
+        const target = $(this).attr('href');
+
+        if ($(target).length) {
+            $('html, body').stop().animate(
+                {
+                    scrollTop: $(target).offset().top
+                },
+                800 // durée en ms
+            );
+        }
+    });
+});
